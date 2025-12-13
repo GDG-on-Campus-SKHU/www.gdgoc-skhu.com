19d57f848589052f9b75e4eb256d62cd3acd314a
Han6262 <hsiyeon@icloud.com>
2025-12-14 01:10:43 +0900

    activity 페이지- api 연동 1차 (#54)
    * Feat/#26 액티비티 화면 구현 (#34)
    
    * 액티비티 - api 연동 1차 (#55)

---
+++ src/features/team-building/components/Activity/ActivitySection.tsx
@@ -0,0 +1,42 @@
+import { css } from '@emotion/react';
+
+import { Activity } from '@/lib/activity.api';
+import ActivityCard from './ActivityCard';
+
+interface ActivitySectionProps {
+  categoryName: string;
+  activities: Activity[];
+}
+
+export default function ActivitySection({ categoryName, activities }: ActivitySectionProps) {
+  return (
+    <div>
+      <h2 css={categoryCss}>{categoryName}</h2>
+      <div css={gridCss}>
+        {activities.map((activity, index) => (
+          <ActivityCard
+            key={`${categoryName}-${index}`}
+            title={activity.title}
+            author={activity.speaker}
+            youtubeId={activity.youtubeId}
+            year={activity.generation}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const categoryCss = css`
+  font-size: 24px;
+  line-height: 160%;
+  color: #111111;
+  margin-bottom: 2.5rem;
+`;
+
+const gridCss = css`
+  margin-top: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+`;
