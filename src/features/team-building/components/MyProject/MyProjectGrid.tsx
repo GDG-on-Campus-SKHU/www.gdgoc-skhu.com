13921ebcc50c8e768f8db19efa20326816c54467
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-12-15 00:09:15 +0900

    fix: api연동2차 코드와 동기화2

--- src/features/team-building/components/MyProject/MyProjectGrid.tsx
+++ src/features/team-building/components/MyProject/MyProjectGrid.tsx
@@ -1,14 +1,40 @@
+import type { MyPageProject } from '@/lib/mypageProject.api';
 import { css } from '@emotion/react';
 
 import { MOCK_PROJECTS } from '../../types/myproject';
 import MyProjectCard from './MyProjectCard';
 
 export default function ProjectGrid() {
-  const projects = MOCK_PROJECTS;
+  const projects: MyPageProject[] = MOCK_PROJECTS.map((p, index) => ({
+    projectId: index + 1,
+    thumbnailUrl: p.thumbnailUrl,
+    projectName: p.title,
+    exhibited: true,
+    description: p.description,
+    isLeader: true,
+    leader: {
+      userId: 1,
+      name: '윤준석',
+      part: 'BACKEND',
+    },
+    members: [
+      {
+        userId: 2,
+        name: '권지후',
+        part: 'PM',
+      },
+      {
+        userId: 3,
+        name: '최인석',
+        part: 'AI/ML',
+      },
+    ],
+  }));
+
   return (
     <div css={gridCss}>
       {projects.map(p => (
-        <MyProjectCard key={p.id} item={p} />
+        <MyProjectCard key={p.projectId} item={p} />
       ))}
     </div>
   );
@@ -17,7 +43,6 @@ export default function ProjectGrid() {
 const gridCss = css`
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
-  // 카드 그리드 반응형 설정
   @media (max-width: 1024px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
