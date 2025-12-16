c2c5d89b27357a04fe97281373fddb8eb550e217
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-12-16 21:42:45 +0900

    hotfix: AdminSubsScreening.tsx 빌드 에러 수정

--- src/pages/AdminSubsScreening.tsx
+++ src/pages/AdminSubsScreening.tsx
@@ -1,8 +1,6 @@
 import dynamic from 'next/dynamic';
 import AdminLayout from '../features/Admin/layout/AdminLayout';
 
-import AdminLayout from '../features/Admin/layout/AdminLayout';
-
 const AdminSubsScreening = dynamic(
   () => import('../features/Admin/components/AdminSubsScreening/AdminSubsScreening'),
   { ssr: false }
