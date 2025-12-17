30ee01b56d39fcac2a492a8d57cae6c6887db854
Park, Daekyeong <pdk2037@gmail.com>
2025-12-18 03:34:05 +0900

    refactor(router): 사이드바 router path 수정

--- src/features/Admin/layout/AdminLayout.tsx
+++ src/features/Admin/layout/AdminLayout.tsx
@@ -32,8 +32,8 @@ type Props = {
 const NAV_ITEMS: NavItem[] = [
   { label: '대시보드', path: '/admin' },
   { label: '가입 심사', path: '/AdminSubsScreening' },
-  { label: '멤버 관리', path: '/admin-member/list' },
-  { label: '프로젝트 관리', path: '/AdminProjectManage' },
+  { label: '멤버 관리', path: '/admin-member' },
+  { label: '프로젝트 관리', path: '/admin-project' },
   { label: '아이디어 관리', path: '/AdminIdea' },
   { label: '프로젝트 갤러리 관리', path: '/AdminProjectGallery' },
   { label: '액티비티 관리', path: '/AdminActivity' },
