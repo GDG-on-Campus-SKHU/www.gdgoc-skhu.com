1cf190bd9ecbe9dd3842440f3b7ec63f19b4acb8
fs6-kde <kde040508@naver.com>
2025-12-18 12:15:13 +0900

    팀빌딩 일정모달 & 등록 시 확인 모달 수정 (#86)
    * 팀빌딩 api 연동 수정 & 마이팀과 프젝갤 버그 수정
    
    * 빌드 에러 수정
    
    * 첫번째 썸네일 추출 & 프젝 작성폼 ui 수정
    
    * 빌드 에러 해결
    
    * 관리자 가입 심사 페이지 승인 & 거절 & 재심사 api 연동
    
    * 빌드 에러 해결
    
    * 팀빌딩 일정모달 수정
    
    * 빌드 에러 해결
    
    ---------
    
    Co-authored-by: fs6-kde <kde040508@gmail.com>

--- src/features/team-building/styles/IdeaForm.tsx
+++ src/features/team-building/styles/IdeaForm.tsx
@@ -481,7 +481,7 @@ export const ModalOverlay = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  z-index: 9999;
+  z-index: 2000;
 `;
 
 export const ModalCard = styled.div`
