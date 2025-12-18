40eafcb84c8ba4219461ffaf2be04e3b98795696
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-12-18 11:55:41 +0900

    fix: 관리자페이지 탭바겹치는문제 해결

--- src/pages/admin-project.tsx
+++ src/pages/admin-project.tsx
@@ -1,16 +1,10 @@
 import dynamic from 'next/dynamic';
 
-import AdminLayout from '../features/Admin/layout/AdminLayout';
-
 const AdminProjectManage = dynamic(
   () => import('../features/Admin/components/AdminProjectManage/AdminProjectManage'),
   { ssr: false }
 );
 
 export default function AdminProjectPage() {
-  return (
-    <AdminLayout>
-      <AdminProjectManage />
-    </AdminLayout>
-  );
+  return <AdminProjectManage />;
 }
